de1755307634a18d96330ec84d5fd99f0d35ba7c
FranxAranda <146369228+FranxAranda@users.noreply.github.com>
2023-11-24 21:38:09 +0100

    Add files via upload

--- pecjava.js
+++ pecjava.js
@@ -24,15 +24,4 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     }
   });
 
-  var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+ 
